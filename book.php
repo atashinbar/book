d3f201dbae3e5f4a2cbeadff418d18bca5983020
Ali Atashinbar <ali.atashinbar@gmail.com>
2024-10-25 01:57:13 +0330

    create db
    - add config file
    - create db after activation plugin

--- book.php
+++ book.php
@@ -9,5 +9,98 @@
  * Author URI:      https://google.com
  * Text Domain:     book
  * Domain Path:     /languages
- * Version:         0.1
+ * Version:         0.2
  */
+
+if (file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
+    require dirname(__FILE__) . '/vendor/autoload.php';
+}
+
+use Rabbit\Application;
+use Rabbit\Plugin;
+use Rabbit\Utils\Singleton;
+use League\Container\Container;
+
+/**
+ * Class BookPluginInit
+ * @package BookPluginInit
+ */
+class BookPluginInit extends Singleton
+{
+    /**
+     * @var Container
+     */
+    private $application;
+
+    /**
+     * BookPluginInit constructor.
+     */
+    public function __construct()
+    {
+        $this->application = Application::get()->loadPlugin(__DIR__, __FILE__, 'config');
+        $this->init();
+    }
+
+    public function init()
+    {
+        try {
+            /**
+             * Activation hooks
+             */
+            $this->application->onActivation(function () {
+                $db_name = $this->application->config('db_name');
+
+                global $wpdb;
+                $table_name = $wpdb->prefix . $db_name;
+                $charset_collate = $wpdb->get_charset_collate();
+
+                $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+                    id mediumint(9) NOT NULL AUTO_INCREMENT,
+                    post_id bigint(20) UNSIGNED NOT NULL,
+                    isbn varchar(255) DEFAULT '' NOT NULL,
+                    PRIMARY KEY  (id)
+                ) $charset_collate;";
+
+                require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+                dbDelta($sql);
+            });
+        } catch (Exception $e) {
+            /**
+             * Print the exception message to admin notice area
+             */
+            add_action('admin_notices', function () use ($e) {
+                AdminNotice::permanent(['type' => 'error', 'message' => $e->getMessage()]);
+            });
+
+            /**
+             * Log the exception to file
+             */
+            add_action('init', function () use ($e) {
+                if ($this->application->has('logger')) {
+                    $this->application->get('logger')->warning($e->getMessage());
+                }
+            });
+        }
+    }
+
+
+    /**
+     * @return Container
+     */
+    public function getApplication()
+    {
+        return $this->application;
+    }
+}
+
+/**
+ * Returns the main instance of BookPluginInit.
+ *
+ * @return BookPluginInit
+ */
+function bookPlugin()
+{
+    return BookPluginInit::get();
+}
+
+bookPlugin();
---
+++ config/index.php
@@ -0,0 +1,23 @@
+<?php
+$config = [
+    /**
+     * Database Name
+     */
+    'db_name'       => 'books_info',
+
+    /**
+     * Post Type Name
+     */
+    'cp_name'        => 'book',
+
+    /**
+     * Taxonomies Name
+     */
+    'taxonomy1_name'        => 'publisher',
+    'taxonomy2_name'        => 'author',
+
+    /**
+     * Metabox Name
+     */
+    'metabox_name'        => 'isbn',
+];
