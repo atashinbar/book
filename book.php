677c8dd623136104226d7b62c4892ceea5e275f2
Ali Atashinbar <ali.atashinbar@gmail.com>
2024-10-25 01:11:23 +0330

    First Commit
    - create plugin and composer

---
+++ book.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name:     Books
+ * Plugin URI:      https://www.veronalabs.com
+ * Plugin Prefix:   book
+ * Description:     This is test plugin
+ * Author:          Ali Atashinbar
+ * Author URI:      https://google.com
+ * Text Domain:     book
+ * Domain Path:     /languages
+ * Version:         0.1
+ */
